754004a09c2fdf79fbb94c57823325e047f7fed2
Pav0l <pavol.karas@gmail.com>
2019-06-18 15:44:10 +0200

    First pass Ancestors

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -32,8 +32,8 @@ class Graph:
 
     def add_nodes_and_edges(self, node_list):
         for i in node_list:
-            parent = i[0]
-            child = i[1]
+            parent = i[1]
+            child = i[0]
 
             if parent not in self.vertices:
                 self.vertices[parent] = set()
@@ -42,8 +42,57 @@ class Graph:
 
             self.vertices[parent].add(child)
 
+    def find_ancestor(self, node):
+
+        ancestor_distance = []
+        earliest_ancestor = None
+
+        q = Queue()
+        q.enqueue([node, ancestor_distance])
+        visited = set()
+
+        print(f'node: {node}, que: {q.queue}, size: {q.size()}')
+
+        while q.size() > 0:
+            # get next item from queue
+            current_node = q.queue[0]
+            cn_val = current_node[0]
+            cn_path = current_node[1]
+
+            print(f'\nNext in queue = {current_node}')
+
+            # check if we did not visit this node yet
+            if cn_val not in visited:
+                visited.add(cn_val)
+
+                print(f'parents: {self.vertices[cn_val]}')
+
+                for parent in self.vertices[cn_val]:
+                    parent_path = cn_path.copy()
+                    parent_path.append(cn_val)
+
+                    if len(parent_path) > len(ancestor_distance):
+                        ancestor_distance = parent_path
+                        if earliest_ancestor == None or parent <= earliest_ancestor:
+                            earliest_ancestor = parent
+
+                    q.enqueue([parent, parent_path])
+
+            print(f'cn path vs anc_dist: {cn_path} - {ancestor_distance}')
+            if len(cn_path) >= len(ancestor_distance):
+                cn_path.append(cn_val)
+                ancestor_distance = cn_path
+                earliest_ancestor = cn_val
+
+            q.dequeue()
+            print(f'anc: {earliest_ancestor}, dist: {ancestor_distance}')
+            print(f'QUE: {q.queue}\n')
+
+        return earliest_ancestor
+
 
 g = Graph()
 g.add_nodes_and_edges(input_list)
 
 print(g.vertices)
+print(g.find_ancestor(input_ID))
